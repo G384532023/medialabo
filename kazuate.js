cc55e47932e7c8c8cb7936d113fcbddffb45d12b
G3844532023 <238453@st.takushoku-u.ac.jp>
2024-05-16 13:53:32 +0900

    特殊

---
+++ kazuate.js
@@ -0,0 +1,50 @@
+// 答え
+let kotae = Math.floor(Math.random() * 10) + 1;
+console.log('答え（デバッグ用）: ' + kotae);
+
+// 入力回数（予想回数）
+let kaisu = 0;
+
+// ボタン要素を取得
+let button = document.getElementById("submitBtn");
+
+// ボタンクリック時の処理を設定
+button.addEventListener("click", hantei);
+
+// ボタンを押した後の処理をする関数 hantei() の定義
+function hantei() {
+  // テキストボックスから値を取得
+  let yoso = document.getElementById("inputNumber").value;
+  yoso = parseInt(yoso); // 文字列から数値への変換
+
+  // 空欄や無効な入力のチェック
+  if (isNaN(yoso) || yoso < 1 || yoso > 10) {
+    console.log("1から10までの数字を入力してください。");
+    return; // 処理を終了
+  }
+  kaisu++;
+  console.log(kaisu+"回目の予想: "+yoso);
+  
+  // 課題3-1: 正解判定する
+  if (yoso === kotae) {
+    if(kaisu >= 4){
+    }else{
+        console.log("正解です、おめでとう!");
+    }
+  } else {
+    if (yoso < kotae && (kaisu === 1 || kaisu === 2)) {
+      console.log("まちがい．答えはもっと大きいですよ");
+    } else if (yoso > kotae && (kaisu == 1 || kaisu == 2)) {
+      console.log("まちがい．答えはもっと小さいですよ");
+    } else if (kaisu === 3) {
+      console.log("まちがい．残念でした、答えは " + kotae + " です．");
+    }
+  }
+  // 入力回数を更新
+
+  // 入力回数が4回に達した場合、終了メッセージを表示
+  if (kaisu >= 4) {
+    console.log("答えは " + kotae + " でした．すでにゲームは終わっています");
+    // ボタンのクリックイベントを削除
+  }
+}
---
+++ project.js
@@ -0,0 +1,403 @@
+// NHKのデータ
+var nhkData = {
+    "list": {
+        "g1": [
+            {
+                "id": "2022030428673",
+                "event_id": "28673",
+                "start_time": "2022-03-04T04:35:00+09:00",
+                "end_time": "2022-03-04T04:40:00+09:00",
+                "area": {
+                    "id": "130",
+                    "name": "東京"
+                },
+                "service": {
+                    "id": "g1",
+                    "name": "ＮＨＫ総合１",
+                    "logo_s": {
+                        "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
+                        "width": "100",
+                        "height": "50"
+                    },
+                    "logo_m": {
+                        "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
+                        "width": "200",
+                        "height": "100"
+                    },
+                    "logo_l": {
+                        "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
+                        "width": "200",
+                        "height": "200"
+                    }
+                },
+                "title": "みんなのうた「ごっつぉさま」／「超変身！ミネラルフォーマーズ」",
+                "subtitle": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
+                "content": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
+                "act": "",
+                "genres": [
+                    "0409",
+                    "0700",
+                    "0504"
+                ]
+            },
+            {
+                "id": "2022030427069",
+                "event_id": "27069",
+                "start_time": "2022-03-04T23:05:00+09:00",
+                "end_time": "2022-03-04T23:10:00+09:00",
+                "area": {
+                    "id": "130",
+                    "name": "東京"
+                },
+                "service": {
+                    "id": "g1",
+                    "name": "ＮＨＫ総合１",
+                    "logo_s": {
+                        "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
+                        "width": "100",
+                        "height": "50"
+                    },
+                    "logo_m": {
+                        "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
+                        "width": "200",
+                        "height": "100"
+                    },
+                    "logo_l": {
+                        "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
+                        "width": "200",
+                        "height": "200"
+                    }
+                },
+                "title": "パラスポーツ×アニメ「アニ×パラ」▽パラアルペンスキーテーマ曲江口寿史×ＡＣＣ",
+                "subtitle": "パラスポーツの魅力をアニメで伝える番組。高速滑走に挑む精神力が試されるパラアルペンスキーを描く。キャラ原案：江口寿史／曲：Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ",
+                "content": "パラスポーツの魅力をアニメで伝えるプロジェクトの第１３弾。圧倒的なスピードに挑む「パラアルペンスキー」の世界を江口寿史原案の魅力的なキャラクターで描く。平昌パラリンピック金メダリストの村岡桃佳選手への取材から生まれた主人公・桃は、スピードへの恐怖を克服していく。その壁を越えた先にあるものとは…　テーマ曲　♪「Ｏｎ　Ｙｏｕｒ　Ｍａｒｋ」はＡｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂが手掛けた。",
+                "act": "【声】松本まりか，【出演】Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ，【監督】西村一彦，【脚本】加納新太，【原案】江口寿史",
+                "genres": [
+                    "0700"
+                ]
+            }
+        ]
+    }
+};
+
+// グルメのデータ
+var gourmetData = {
+    "results": {
+        "api_version": "1.26",
+        "results_available": 52,
+        "results_returned": "10",
+        "results_start": 1,
+        "shop": [
+          {
+            "access": "京王八王子駅を背にし右手に見えるローソンの隣のビル、ホテルザ・ビーの2階です。",
+            "address": "東京都八王子市明神町４-6-12　ホテル・ザ・ビー八王子（旧八王子プラザホテル）2F",
+            "band": "可",
+            "barrier_free": "なし",
+            "budget": {
+              "average": "ディナー3000円",
+              "code": "B003",
+              "name": "3001～4000円"
+            },
+            "budget_memo": "",
+            "capacity": 100,
+            "card": "利用可",
+            "catch": "【サプライズ演出有】 結婚パーティー受付中",
+            "charter": "貸切可 ：VIPルーム1部屋2～15名様前後（完全個室）/パーティーフロア！50型モニター・スポットライト等充実",
+            "child": "お子様連れOK",
+            "close": "不定休有り。店休時は店長携帯電話09035324825まで問い合わせください♪",
+            "coupon_urls": {
+              "pc": "https://www.hotpepper.jp/strJ000989843/map/?vos=nhppalsa000016",
+              "sp": "https://www.hotpepper.jp/strJ000989843/scoupon/?vos=nhppalsa000016"
+            },
+            "course": "あり",
+            "english": "なし",
+            "free_drink": "あり",
+            "free_food": "なし",
+            "genre": {
+              "catch": "大人の社交場非日常を味わうダイニング",
+              "code": "G001",
+              "name": "居酒屋"
+            },
+            "horigotatsu": "なし",
+            "id": "J000989843",
+            "karaoke": "あり",
+            "ktai_coupon": 0,
+            "large_area": {
+              "code": "Z011",
+              "name": "東京"
+            },
+            "large_service_area": {
+              "code": "SS10",
+              "name": "関東"
+            },
+            "lat": 35.6585460152,
+            "lng": 139.34327231,
+            "logo_image": "https://imgfp.hotp.jp/IMGH/21/04/P038512104/P038512104_69.jpg",
+            "lunch": "なし",
+            "middle_area": {
+              "code": "Y110",
+              "name": "八王子・立川"
+            },
+            "midnight": "営業していない",
+            "mobile_access": "京王八王子駅1分/JR八王子駅北口5分",
+            "name": "バグダッドカフェ Bagdadcafe/モータウン MOTOWN",
+            "name_kana": "ばぐだっどかふぇもーたうんはちおうじ",
+            "non_smoking": "禁煙席なし",
+            "open": "月～日、祝日、祝前日: 17:00～21:00 （料理L.O. 20:00 ドリンクL.O. 20:00）",
+            "other_memo": "ステージ・マイク・カラオケ等",
+            "parking": "なし",
+            "party_capacity": 250,
+            "pet": "不可",
+            "photo": {
+              "mobile": {
+                "l": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_168.jpg",
+                "s": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_100.jpg"
+              },
+              "pc": {
+                "l": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_238.jpg",
+                "m": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_168.jpg",
+                "s": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_58_s.jpg"
+              }
+            },
+            "private_room": "あり ：VIPルーム1部屋2～15名様前後（完全個室）/パーティーフロア",
+            "service_area": {
+              "code": "SA11",
+              "name": "東京"
+            },
+            "shop_detail_memo": "ご質問、ご要望はお気軽にお問い合わせください♪",
+            "show": "あり",
+            "small_area": {
+              "code": "X220",
+              "name": "八王子"
+            },
+            "station_name": "京王八王子",
+            "sub_genre": {
+              "code": "G002",
+              "name": "ダイニングバー・バル"
+            },
+            "tatami": "なし",
+            "tv": "あり",
+            "urls": {
+              "pc": "https://www.hotpepper.jp/strJ000989843/?vos=nhppalsa000016"
+            },
+            "wedding": "大歓迎！ステージ・マイク・音響・映像等、設備充実！！最大200名様まで是非どうぞ★",
+            "wifi": "あり"
+          },
+          {
+            "access": "ＪＲ八王子駅北口徒歩1分/京王八王子駅徒歩3分",
+            "address": "東京都八王子市東町12-14",
+            "band": "不可",
+            "barrier_free": "あり ：スタッフがお手伝いさせて頂きます。ご不明な点等お気軽にお声掛け下さい。事前のお問い合わせも◎",
+            "budget": {
+              "average": "2,000円(通常平均) 3,000円(宴会平均) ",
+              "code": "B002",
+              "name": "2001～3000円"
+            },
+            "budget_memo": "",
+            "capacity": 100,
+            "card": "利用可",
+            "catch": "2.5時間飲み放題付2000円~ 【★少人数様ソファー★】",
+            "charter": "貸切可 ：八王子で人気の個室バル最大50名様まで対応できます。20名様～フロア貸切応相談　最大100名様までOK",
+            "child": "お子様連れ歓迎 ：チャイルドシートもご用意しています。お子様連れのママ会・ご家族でのご利用にもお気軽にどうぞ♪",
+            "close": "【年中無休】貸切宴会のご予約も絶賛受付中！最大100名様までご利用可能ですので、会社宴会 同窓会 宴会にオススメ♪",
+            "coupon_urls": {
+              "pc": "https://www.hotpepper.jp/strJ001275177/map/?vos=nhppalsa000016",
+              "sp": "https://www.hotpepper.jp/strJ001275177/scoupon/?vos=nhppalsa000016"
+            },
+            "course": "あり",
+            "english": "なし",
+            "free_drink": "あり ：★NEW OPEN★当店イチ押しのコース★2.5h飲み放題付き「Funny Classicコース」4980円→2980円",
+            "free_food": "あり ：★NEW OPEN★当店イチ押しのコース★2.5h飲み放題付き「Funny Classicコース」4980円→2980円",
+            "genre": {
+              "catch": "八王子 個室居酒屋 飲み放題 肉バル 女子会",
+              "code": "G001",
+              "name": "居酒屋"
+            },
+            "horigotatsu": "なし ：温かな照明と開放的な店内で楽しくご宴会♪八王子での宴会 飲み会 女子会 二次会 合コンに◎",
+            "id": "J001275177",
+            "karaoke": "なし",
+            "ktai_coupon": 0,
+            "large_area": {
+              "code": "Z011",
+              "name": "東京"
+            },
+            "large_service_area": {
+              "code": "SS10",
+              "name": "関東"
+            },
+            "lat": 35.6582259169,
+            "lng": 139.3381858122,
+            "logo_image": "https://imgfp.hotp.jp/IMGH/61/98/P038366198/P038366198_69.jpg",
+            "lunch": "なし",
+            "middle_area": {
+              "code": "Y110",
+              "name": "八王子・立川"
+            },
+            "midnight": "営業している",
+            "mobile_access": "JR八王子駅北口徒歩1分/京王八王子駅徒歩3分",
+            "name": "隠れ家バル Funny&Bouquet",
+            "name_kana": "かくれやばる　ふぁにーあんどぶーけ",
+            "non_smoking": "禁煙席なし",
+            "open": "月～日、祝日、祝前日: 17:00～翌5:00 （料理L.O. 翌3:00 ドリンクL.O. 翌4:00）",
+            "other_memo": "個室や貸切の詳細等お気軽にお電話にてお問い合わせください。八王子での飲み会に◎",
+            "parking": "なし ：お近くのコインパーキングをご利用下さい。",
+            "party_capacity": 50,
+            "pet": "不可",
+            "photo": {
+              "mobile": {
+                "l": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_168.jpg",
+                "s": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_100.jpg"
+              },
+              "pc": {
+                "l": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_238.jpg",
+                "m": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_168.jpg",
+                "s": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_58_s.jpg"
+              }
+            },
+            "private_room": "なし ：個室　2名×4室　4名×8室　8名～10名×4室　20名～40名×2室",
+            "service_area": {
+              "code": "SA11",
+              "name": "東京"
+            },
+            "shop_detail_memo": "★NEW OPEN★当店イチ押しのコース★2.5h飲み放題付き「Funny Classicコース」4980円→2980円",
+            "show": "なし",
+            "small_area": {
+              "code": "X220",
+              "name": "八王子"
+            },
+            "station_name": "八王子",
+            "sub_genre": {
+              "code": "G002",
+              "name": "ダイニングバー・バル"
+            },
+            "tatami": "なし ：八王子で人気のお座敷個室！最大50名様まで対応できます。",
+            "tv": "あり",
+            "urls": {
+              "pc": "https://www.hotpepper.jp/strJ001275177/?vos=nhppalsa000016"
+            },
+            "wedding": "八王子での結婚式の二次会や同窓会、打ち上げパーティー等多様なシーンにご利用下さい",
+            "wifi": "あり"
+        }
+    ]}
+    };
+    
+
+// 天気のデータ
+var weatherData = {
+    "coord": {
+        "lon": 116.3972,
+        "lat": 39.9075
+    },
+    "weather": [
+        {
+            "id": 803,
+            "main": "Clouds",
+            "description": "曇りがち",
+            "icon": "04d"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 9.94,
+        "feels_like": 8.65,
+        "temp_min": 9.94,
+        "temp_max": 9.94,
+        "pressure": 1022,
+        "humidity": 14,
+        "sea_level": 1022,
+        "grnd_level": 1016
+    },
+    "visibility": 10000,
+    "wind": {
+        "speed": 2.65,
+        "deg": 197,
+        "gust": 4.84
+    },
+    "clouds": {
+        "all": 53
+    },
+    "dt": 1646542386,
+    "sys": {
+        "type": 1,
+        "id": 9609,
+        "country": "CN",
+        "sunrise": 1646520066,
+        "sunset": 1646561447
+    },
+    "timezone": 28800,
+    "id": 1816670,
+    "name": "北京市",
+    "cod": 200
+};
+
+function searchData() {
+    var searchKeyword = document.getElementById("search").value;
+    var resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = ""; // 検索結果をリセット
+
+    switch (searchKeyword) {
+        case "NHK":
+            displayNHKData(resultDiv);
+            break;
+        case "グルメ":
+            displayGourmetData(resultDiv);
+            break;
+        case "天気":
+            displayWeatherData(resultDiv);
+            break;
+        default:
+            resultDiv.innerHTML = "<div class='no-result-box'><p class='no-result'>該当するデータが見つかりませんでした。</p></div>";
+    }
+}
+
+function displayNHKData(resultDiv) {
+    var nhkHTML = "<div class='data-box'><h2>NHKデータ</h2>";
+    nhkHTML += "<table>";
+    nhkHTML += "<tr><th>番組名</th><th>放送開始時間</th><th>放送終了時間</th><th>放送エリア</th></tr>";
+    nhkData.list.g1.forEach(function(program) {
+        nhkHTML += "<tr>";
+        nhkHTML += "<td>" + program.title + "</td>";
+        nhkHTML += "<td>" + program.start_time + "</td>";
+        nhkHTML += "<td>" + program.end_time + "</td>";
+        nhkHTML += "<td>" + program.area.name + "</td>";
+        nhkHTML += "</tr>";
+    });
+    nhkHTML += "</table></div>";
+    resultDiv.innerHTML = nhkHTML;
+}
+
+function displayGourmetData(resultDiv) {
+    var gourmetHTML = "<div class='data-box'><h2>グルメデータ</h2>";
+    gourmetHTML += "<table>";
+    gourmetHTML += "<tr><th>項目名</th><th>項目の説明</th></tr>";
+
+    gourmetData.results.shop.forEach(function(shop) {
+        gourmetHTML += "<tr><td>アクセス情報</td><td>" + shop.access + "</td></tr>";
+        gourmetHTML += "<tr><td>住所</td><td>" + shop.address + "</td></tr>";
+        gourmetHTML += "<tr><td>予算</td><td>" + shop.budget.name + "</td></tr>";
+        gourmetHTML += "<tr><td>キャッチコピー</td><td>" + shop.catch + "</td></tr>";
+        gourmetHTML += "<tr><td>ジャンル</td><td>" + shop.genre.name + "</td></tr>";
+        gourmetHTML += "<tr><td>店舗名</td><td>" + shop.name + "</td></tr>";
+        gourmetHTML += "<tr><td>営業日時</td><td>" + shop.open + "</td></tr>";
+        gourmetHTML += "<tr><td>最寄駅</td><td>" + shop.station_name + "</td></tr>";
+        gourmetHTML += "<tr><td>サブジャンルの名前</td><td>" + shop.sub_genre.name + "</td></tr>";
+        gourmetHTML += "<tr><td>店舗画像</td><td><img src='" + shop.photo.mobile.l + "' alt='店舗画像'></td></tr>";
+        gourmetHTML += "<tr><td>ロゴ</td><td><img src='" + shop.logo_image + "' alt='ロゴ'></td></tr>";
+        gourmetHTML += "<tr><td>マップ</td><td><a href='" + shop.urls.pc + "' target='_blank'>Google Map</a></td></tr>";
+        gourmetHTML += "<tr><td colspan='2'><hr></td></tr>";
+    });
+
+    gourmetHTML += "</table></div>";
+    resultDiv.innerHTML = gourmetHTML;
+}
+
+
+function displayWeatherData(resultDiv) {
+    var weatherHTML = "<div class='data-box'><h2>天気データ</h2>";
+    weatherHTML += "<p>天気: " + weatherData.weather[0].description + "</p>";
+    weatherHTML += "<p>気温: " + weatherData.main.temp + "℃</p>";
+    weatherHTML += "<p>湿度: " + weatherData.main.humidity + "%</p>";
+    weatherHTML += "<p>風速: " + weatherData.wind.speed + " m/s</p>";
+    weatherHTML += "</div>";
+    resultDiv.innerHTML = weatherHTML;
+}
